b164b962e2738f44dd66401f973404906316e111
jlsears <jen.lauren.sears@gmail.com>
2015-11-21 10:49:27 -0600

    initial commit

---
+++ beerswap/beerswap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(beerswap.Startup))]
+namespace beerswap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
